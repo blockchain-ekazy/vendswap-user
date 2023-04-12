7b48589dc3d99d85816aa3c8c5f607754254dd85
Muhammad Sabir Junaid <100062048+blockchain-ekazy@users.noreply.github.com>
2023-04-13 01:30:03 +0500

    Update config.js

--- src/blockchain/config.js
+++ src/blockchain/config.js
@@ -1,9 +1,9 @@
 // prettier-ignore
-export const vendswap = "0xCbA8770F62ca424a2F085CE8bc70ac93cF549F73";
-export const usdt = "0x7c87561b129f46998fc9Afb53F98b7fdaB68696f";
-export const usdc = "0x156bE79B809DE4C0776445Ff1a50817E24c1f4D4";
+export const vendswap = "0x5207a615e7b9abedcd9bac3367f03530e0b7132d";
+export const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+export const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const usdtAMOUNT = 500 * 1e6;
-export const NETWORK = "5";
-export const NETWORKNAME = "Goerli";
+export const NETWORK = "1";
+export const NETWORKNAME = "Ethereum";
 export const INFURA =
-  "https://goerli.infura.io/v3/bd3283c53475479792ed8f68c398a0e9";
+  "https://mainnet.infura.io/v3/bd3283c53475479792ed8f68c398a0e9";

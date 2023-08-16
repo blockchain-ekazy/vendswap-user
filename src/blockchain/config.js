0a1327f9c3a5a8a16cbae7f1f093905da481a129
arazxa <blockchain.ekazy@gmail.com>
2023-08-16 06:38:13 +0500

    Update config.js

--- src/blockchain/config.js
+++ src/blockchain/config.js
@@ -2,7 +2,7 @@
 export const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const vendswap = "0x5207a615e7b9abedcd9bac3367f03530e0b7132d";
 export const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const usdtAMOUNT = 500 * 1e6;
+export const usdtAMOUNT = 0.05 * 1e6;
 export const NETWORK = "1";
 export const NETWORKNAME = "Ethereum";
 export const INFURA =
